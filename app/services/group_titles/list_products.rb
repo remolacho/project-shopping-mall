6a51bc83c9eaa9d625872df676dc7ee78be56cea
jrojasetiner <fiona.83>
2020-12-15 09:16:32 -0500

    se crea el test y la documentacion para el EP por titulos se crea la ruta para los productos por titulo

---
+++ app/services/group_titles/list_products.rb
@@ -0,0 +1,38 @@
+# hereda de Categories::ListProducts y sobreescribe el metodo de jerarquia para que adapte
+# al del titulo y sus categorias hijas
+
+class GroupTitles::ListProducts < ::Categories::ListProducts
+
+  attr_accessor :group_title
+
+  def initialize(group_title:, data:)
+    super(category: nil, data: data)
+    @group_title = group_title
+  end
+
+  def perform
+    products = list
+    products = filter_brand(products)
+    products = filter_rating(products)
+    products = filter_prices(products)
+    products = filter_order(products)
+    products = pagination(products)
+
+    {
+      success: true,
+      per_page: ENV['PER_PAGE'].to_i,
+      total_pages: products.total_pages,
+      total_objects: products.total_count,
+      products: serializer(products)
+    }
+  end
+
+  private
+
+  def hierarchy
+    categories = group_title.categories
+    raise ActiveRecord::RecordNotFound, 'No hay categorias para este titulo' unless categories.present?
+
+    categories.map(&:id) | categories.map{|category| category.children.ids}.flatten
+  end
+end
---
+++ spec/requests/v1/group_titles/products_controller_spec.rb
@@ -0,0 +1,66 @@
+require 'swagger_helper'
+
+RSpec.describe V1::Categories::ProductsController, type: :request do
+  include_context 'user_stuff'
+  include_context 'meta_data_stuff'
+  include_context 'company_stuff'
+  include_context 'store_stuff'
+  include_context 'products_stuff'
+
+  describe 'Retorna una lista de productos por categoria' do
+
+    path "/v1/groupTitles/{title_id}/products" do
+      get 'Lista de productos' do
+        tags 'Zofri titulos'
+        description "retorna la lista de productos por las categorias del titulo seleccionado, en su filtro puede estar por rango de precio,
+<p>ordenado por precio asc o desc, marca, calificacion Qyery ?prices=['1000-2000', '2000-5000']&brand_ids=[1, 2, 3]&order_by=DESC&rating=3</p>"
+        produces 'application/json'
+        parameter name: 'secret-api', in: :header, required: true
+        parameter name: :title_id, in: :path
+        parameter name: :brand_ids, in: :query, required: false, type: :string
+        parameter name: :prices, in: :query, required: false, type: :string
+        parameter name: :order_by, in: :query, required: false, type: :string
+        parameter name: :rating, in: :query, required: false, type: :integer
+        parameter name: :page, in: :query, required: false, type: :integer
+        response 200, 'success!!!' do
+          schema type: :object,
+                 properties: {
+                     success: { type: :boolean },
+                     per_page: { type: :integer, default: 12 },
+                     total_pages: { type: :integer, default: 4 },
+                     total_objects: { type: :integer, default: 40 },
+                     products: { type: :array,
+                                 items: {
+                                     type: :object,
+                                     properties: {
+                                         id: { type: :integer },
+                                         name: { type: :string },
+                                         category_name: { type: :string },
+                                         short_description: { type: :string },
+                                         price: { type: :number },
+                                         rating: { type: :number },
+                                         image_url: { type: :string, nullable: true },
+                                         brand_name: { type: :string },
+                                     }
+                                 }
+                     }
+                 }
+
+          let(:title_id) { group_titles.last.id }
+          run_test!
+        end
+
+        response 404, 'Error data no encontrada' do
+          schema type: :object,
+                 properties: {
+                     success: {type: :boolean, default: false},
+                     message: {type: :string}
+                 }
+
+          let(:title_id) { title_without_categories.id }
+          run_test!
+        end
+      end
+    end
+  end
+end
