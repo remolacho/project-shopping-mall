211a9c60889c1e16a9dc56c5b4a44b7e946ca6b7
paulotijero <paulotijero@gmail.com>
2021-01-06 13:01:36 -0500

    ADD: update profile

--- app/controllers/users/profile_controller.rb
+++ app/controllers/users/profile_controller.rb
@@ -2,4 +2,15 @@ class Users::ProfileController < ApplicationController
   def me
     render json: {success: true, user: UserSerializer.new(current_user)}, status: 200
   end
+
+  def update
+    current_user.update!(allowed_params)
+    render json: {success: true, user: UserSerializer.new(current_user)}, status: :ok
+  end
+
+  private 
+
+  def allowed_params
+    params.require(:user).permit(:name, :lastname, :rut, :email, :gender, :birthdate)
+  end
 end
