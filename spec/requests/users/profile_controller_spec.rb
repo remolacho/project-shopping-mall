45f26c7ff9bd07211564ca960a988bb56c9d2330
paulotijero <paulotijero@gmail.com>
2021-01-06 13:30:48 -0500

    ADD: test swagger to update user

--- spec/requests/users/profile_controller_spec.rb
+++ spec/requests/users/profile_controller_spec.rb
@@ -64,6 +64,83 @@ RSpec.describe Users::ProfileController, type: :request do
         end
       end
     end
+
+    path "/users/profile" do
+      put 'Actualiza la data del usuario' do
+        tags 'Zofri Users'
+        description 'retorna los datos del usuario actualizado y se debe enviar el jwt en la cabecera de solicitud "Authorization"'
+        consumes 'application/json'
+        produces 'application/json'
+        parameter name: 'Authorization', in: :header, required: true
+        parameter name: 'secret-api', in: :header
+        parameter name: :user_data, in: :body, schema: {
+          type: :object,
+          properties: {
+            user: {
+              type: :object,
+              properties: {
+                name: { type: :string },
+                lastname: { type: :string },
+                email: { type: :string }
+              }
+            }
+          }
+        }
+
+        response 200, 'Success!!' do
+          schema type: :object,
+            properties: {
+              success: {type: :boolean},
+              message: {type: :string},
+              user: { 
+                type: :object,
+                properties: { 
+                  id: { type: :integer },
+                  email: { type: :string },
+                  name: { type: :string },
+                  rut: { type: :string },
+                  image: {type: :string, nullable: true },
+                  phone: {type: :string, nullable: true },
+                  gender: {type: :string, nullable: true},
+                  birthdate: {type: :string, nullable: true},
+                  address: {
+                    type: :object,
+                    properties: {
+                      street_number: {type: :string, nullable: true },
+                      street: {type: :string, nullable: true },
+                      condominium: {type: :string, nullable: true },
+                      apartment_number: {type: :string, nullable: true }
+                    }
+                  },
+                  commune: {
+                    type: :object,
+                    properties: {
+                      id: {type: :integer, nullable: true },
+                      name: {type: :string, nullable: true }
+                    }
+                  }
+                }
+              }
+            }
+
+          let(:'Authorization') {auth_bearer(current_user, {})}
+          let(:user_data){{ user: { name: 'Paulo' } }}
+
+          run_test! do |response|
+            expected_user = JSON.parse(response.body)
+            expect(response.status).to eq(200)
+            expect(expected_user['user']['name']).to eq('Paulo')
+          end
+        end
+
+        response 401, 'JWT error!!!' do
+          let(:'Authorization') { 'Bearer tokenErroe-125' }
+          let(:user_data){{ user: { name: 'Paulo' } }}
+         
+          run_test!
+        end
+      end
+    end
   end
 end
 
