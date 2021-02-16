cd076e9e37fec340bd139e8a6c9963ced95cde79
paulotijero <paulotijero@gmail.com>
2021-02-16 17:32:58 -0500

    DOCS: generate documtation

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: wishlists
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  product_id :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_wishlists_on_product_id  (product_id)
+#  index_wishlists_on_user_id     (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (product_id => products.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class Wishlist < ApplicationRecord
   belongs_to :user
   belongs_to :product
