f16c9d455a7473d248e587569f444e1528775309
Enzo Zerega <eczerega@uc.cl>
2021-03-01 19:38:12 +0000

    Revert "Fix/nb2zx2 order (pull request #74)" Fix debe ir a master

--- app/controllers/v1/orders/completed_controller.rb
+++
@@ -1,9 +0,0 @@
-class V1::Orders::CompletedController < ApplicationController
-  skip_before_action :authenticate_user!
-
-  # GET /v1/orders/completed/8b7f2a6bfd2785f76f1f45373b0f3151
-  def show
-    service = ShoppingCart::Items.new(user: with_user, order: order_completed)
-    render json: service.perform, status: 200
-  end
-end
