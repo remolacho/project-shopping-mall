70ed964cc5a9caf0e92a43ace33c43abb19bd33b
Enzo Zerega <cristobal@Enzos-MacBook-Pro.local>
2021-03-01 16:54:41 -0300

    [Fix] :amazon on development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   config.session_store :cache_store, key: ENV['APP_SESSION_KEY']
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
