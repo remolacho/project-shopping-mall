6320df89c2c7e48d0abdc544b55ece2bd5e82bcb
Diego Carrasco <diego.carrasco@outlook.com>
2021-03-18 04:57:16 -0300

    move seller payment task to api

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_132127) do
+ActiveRecord::Schema.define(version: 2021_03_15_055543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
