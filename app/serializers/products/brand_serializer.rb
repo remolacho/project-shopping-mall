c63022b033d24179950fe4285a9884aea8dac499
remolacho <jonathang_25@gmail.com>
2021-04-27 10:50:20 -0500

    se agregan listan las marcas con los productos de la tienda que esten activos

--- app/serializers/products/brand_serializer.rb
+++ app/serializers/products/brand_serializer.rb
@@ -1,12 +1,7 @@
 class Products::BrandSerializer < ActiveModel::Serializer
   attributes :id, :name, :slug, :products_count, :is_visible
 
-  def products_count
-    return @count_products if @count_products.present?
-    @count_products = Product.counter_by_brand(object.id)
-  end
-
   def is_visible
-    !products_count.zero?
+    true
   end
 end
