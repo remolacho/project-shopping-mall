84cfbb20a49d442d6f51de37df5eaed6e262002d
paulotijero <paulotijero@gmail.com>
2021-04-29 15:44:02 -0500

    FIX: change text in subject

--- app/mailers/order_delivered_mailer.rb
+++ app/mailers/order_delivered_mailer.rb
@@ -9,7 +9,7 @@ class OrderDeliveredMailer < ApplicationMailer
     @shipment = @order.shipment
     @products = list(@order.order_items)
     @email_to = Rails.env.production? ? @order.user_data['email'] : ENV['EMAIL_TO']
-    mail(to: @email_to, subject: '¡Opina sobre tu compra en Zofrishop.cl! ⭐️'.freeze)
+    mail(to: @email_to, subject: '¡Tu pedido ha sido entregado! Opina sobre tu compra en Zofrishop.cl ⭐️'.freeze)
   rescue StandardError => e
     @has_error = true
     @message = e.to_s
