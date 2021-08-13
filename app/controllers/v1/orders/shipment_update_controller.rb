9e8955c14c50dcd2d34df3b66f363ac93ef1bcea
Diego Carrasco <diego.carrasco@outlook.com>
2021-08-13 15:17:32 -0400

    tweak enviame webhook

--- app/controllers/v1/orders/shipment_update_controller.rb
+++ app/controllers/v1/orders/shipment_update_controller.rb
@@ -10,16 +10,11 @@ class V1::Orders::ShipmentUpdateController < ApplicationController
 
       order = Order.find_by(number_ticket: params[:order_number])
 
-      case params[:status_name]
-      when "Listo para despacho - Impreso"
-        order.update(delivery_state: "Listo para retiro")
-        order.store_orders.map do |so|
-          so.update(delivery_state: "En gestión de envío")
-        end
-
+      case params[:status_name].downcase
+      when "listo para despacho - impreso"
         OrderLog.new(order_id: order.id, log: "enviame (#{params[:tracking_number]}): #{params[:status_information]}" ).save
         Payment::Whatsapp::Shipment::Customer.new(order: order, call_method: 'in_shipping_management').call
-      when "Planta de origen", "En reparto"
+      when "planta de origen", "en reparto"
         order.update(delivery_state: "En tránsito")
         order.store_orders.where.not(state: "canceled").map do |so|
           so.update(delivery_state: "En tránsito")
@@ -27,7 +22,7 @@ class V1::Orders::ShipmentUpdateController < ApplicationController
 
         OrderLog.new(order_id: order.id, log: "enviame (#{params[:tracking_number]}): #{params[:status_information]}" ).save
         Payment::Whatsapp::Shipment::Customer.new(order: order, call_method: 'in_transit').call
-      when "Entregado"
+      when "entregado"
         order.update(delivery_state: "Entregado")
         order.store_orders.where.not(state: "canceled").map do |so|
           so.update(delivery_state: "Entregado")
