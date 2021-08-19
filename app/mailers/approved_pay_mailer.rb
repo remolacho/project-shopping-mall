3666425392a2f9006984a10b24cedba8b2cc79ea
Diego Carrasco <diego.carrasco@outlook.com>
2021-08-19 14:40:32 -0400

    FEATURE add invoice pdf to approved payment

--- app/mailers/approved_pay_mailer.rb
+++ app/mailers/approved_pay_mailer.rb
@@ -9,7 +9,10 @@ class ApprovedPayMailer < ApplicationMailer
 
   def customer(to:, meta_data:)
     return unless can_send_email?
-
+    @order = Order.find(number_ticket: meta_data[:number_ticket])
+    attachments["orden_#{@order.number_ticket}.pdf"] = WickedPdf.new.pdf_from_string(
+      render_to_string(pdf: 'invoice', template: 'pdf/invoice.pdf.erb'), { :hash_with_wickedpdf_options }
+    )
     @meta_data = meta_data
     @email_to = Rails.env.production? ? to : ENV['EMAIL_TO']
     mail(to: @email_to, subject: '¡Gracias por comprar en ZofriShop! Estamos preparando tu orden 🚚📦'.freeze)
